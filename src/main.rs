4a12aebe9f70233fa26a5efbf9a26ec05449303b
Marcos Pablo <marcos.almeida@reap.hk>
2026-05-23 02:28:19 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut cmd = String::new();
+    std::io::stdin()
+        .read_line(&mut cmd)
+        .expect("Error reading input");
+
+    let cmd = cmd.trim();
+
+    println!("{cmd}: command not found");
 }
