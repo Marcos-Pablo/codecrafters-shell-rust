d7b5ab1e1115f1682324c463c05c5f5dd49bd0a5
Marcos Pablo <marcos.almeida@reap.hk>
2026-05-23 02:33:53 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     loop {
@@ -11,6 +12,9 @@ fn main() {
             .read_line(&mut command)
             .expect("Error reading input");
 
-        println!("{}: command not found", command.trim());
+        match command.trim() {
+            "exit" => exit(0),
+            _ => println!("{}: command not found", command.trim()),
+        }
     }
 }
